50f65767fb1170644c60822fa03effdc324ff3db
ALicia9j <106652230+ALicia9j@users.noreply.github.com>
2023-07-31 11:13:12 +0800

    Update insert-subCategory.php

--- SBCT/Admin/insert-subCategory.php
+++ SBCT/Admin/insert-subCategory.php
@@ -31,6 +31,8 @@ if (isset($_POST['insert_subcat'])) {
     <link rel="stylesheet" href="../font-awesome-4.7.0/css/font-awesome.min.css" type="text/css">
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"></script>
+    <title>Insert Subcategory</title>
+    <link rel="icon" type="image/jpg" href="../images/logo-dark.jpg">
 </head>
 <body>
 <h2 class="text-center">Insert Subcategory</h2>

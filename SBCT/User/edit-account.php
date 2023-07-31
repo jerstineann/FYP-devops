7c05ffad432e3e56b1260896e2fa1390f63239fc
ALicia9j <106652230+ALicia9j@users.noreply.github.com>
2023-07-31 17:35:18 +0800

    Update edit-account.php

--- SBCT/User/edit-account.php
+++ SBCT/User/edit-account.php
@@ -47,7 +47,7 @@ if (isset($_POST['user_update'])) {
     <link rel="icon" type="image/jpg" href="../images/logo-dark.jpg">
     <link rel="stylesheet" href="user-style.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
-    <link rel="stylesheet" href="font-awesome-4.7.0/css/font-awesome.min.css" type="text/css">
+    <link rel="stylesheet" href="../font-awesome-4.7.0/css/font-awesome.min.css" type="text/css">
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"></script>
 </head>

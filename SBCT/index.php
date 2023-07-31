ee829f861e4fe4093eaeb0ef504635746bd1c7b1
ALicia9j <106652230+ALicia9j@users.noreply.github.com>
2023-07-31 11:21:48 +0800

    Update index.php

--- SBCT/index.php
+++ SBCT/index.php
@@ -7,6 +7,7 @@
 <html lang="en">
 <head>
     <title>SBC | Pet Ecommerce Website</title>
+    <link rel="icon" type="image/jpg" href="images/logo-dark.jpg">
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">

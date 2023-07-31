83ff0e1cf294fd25b429514dd228c398d116a2fd
ALicia9j <106652230+ALicia9j@users.noreply.github.com>
2023-07-31 11:17:34 +0800

    Update recover_forgot.php

--- SBCT/User/recover_forgot.php
+++ SBCT/User/recover_forgot.php
@@ -1,34 +1,32 @@
-<?php session_start() ?>
-<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
-<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<!------ Include the above in your HEAD tag ---------->
-
+<?php 
+session_start(); 
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+require '../vendor/autoload.php';
+?>
 <!doctype html>
 <html lang="en">
 <head>
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
     <!-- Fonts -->
     <link rel="dns-prefetch" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
-
     <link rel="stylesheet" href="style.css">
-
-    <link rel="icon" href="Favicon.png">
-
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
-
     <title>Login Form</title>
+    <link rel="icon" type="image/jpg" href="../images/logo-dark.jpg">
 </head>
 <body>
-
-<nav class="navbar navbar-expand-lg navbar-light navbar-laravel">
+<nav class="navbar navbar-expand-lg navbar-light bg-secondary">
     <div class="container">
-        <a class="navbar-brand" href="#">User Password Recover</a>
+        <a class="navbar-brand" href="#">User Password Recovery</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -37,11 +35,11 @@
 </nav>
 
 <main class="login-form">
-    <div class="cotainer">
+    <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Password Recover</div>
+                <div class="card mt-5">
+                    <div class="card-header">Password Recovery</div>
                     <div class="card-body">
                         <form action="#" method="POST" name="recover_psw">
                             <div class="form-group row">
@@ -52,7 +50,7 @@
                             </div>
 
                             <div class="col-md-6 offset-md-4">
-                                <input type="submit" value="Recover" name="recover">
+                                <input type="submit" value="Recover" name="recover" style="background-color: #E799A3; color: white;  border: none;">
                             </div>
                     </div>
                     </form>
@@ -60,18 +58,15 @@
             </div>
         </div>
     </div>
-    </div>
-
 </main>
 </body>
 </html>
-
 <?php 
     if(isset($_POST["recover"])){
-        include('indata/connect.php');
+        include('../indata/connect.php');
         $email = $_POST["email"];
 
-        $sql = mysqli_query($connect, "SELECT * FROM user_table WHERE user_email='$email'");
+        $sql = mysqli_query($con, "SELECT * FROM user_table WHERE user_email='$email'");
         $query = mysqli_num_rows($sql);
   	    $fetch = mysqli_fetch_assoc($sql);
 
@@ -81,7 +76,7 @@
                 alert("<?php  echo "Sorry, no emails exists "?>");
             </script>
             <?php
-        }else if($fetch["status"] == 0){
+        }else if($fetch["user_email_status"] == "Not verified"){
             ?>
                <script>
                    alert("Sorry, your account must verify first, before you recover your password !");
@@ -91,29 +86,21 @@
         }else{
             // generate token by binaryhexa 
             $token = bin2hex(random_bytes(50));
-
-            //session_start ();
             $_SESSION['token'] = $token;
             $_SESSION['email'] = $email;
 
-            require "Mail/phpmailer/PHPMailerAutoload.php";
-            $mail = new PHPMailer;
+            $mail = new PHPMailer(true);
 
-            $mail->isSMTP();
-            $mail->Host='smtp.gmail.com';
-            $mail->Port=587;
-            $mail->SMTPAuth=true;
-            $mail->SMTPSecure='tls';
-
+            $mail->isSMTP();                            //Send using SMTP
+            $mail->Host = 'smtp.gmail.com';           //Set the SMTP server to send through
+            $mail->SMTPAuth = true;                     //Enable SMTP authentication
+            $mail->Username = 'dammyfyp@gmail.com';       //SMTP username
+            $mail->Password = 'xryymaffhaqzdlfg';                 //SMTP password
+            $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS; //Enable implicit TLS encryption
+            $mail->Port = 465;  
             // h-hotel account
-            $mail->Username='email account';
-            $mail->Password='email password';
-
-            // send by h-hotel email
-            $mail->setFrom('email', 'Password Reset');
-            // get email from input
+            $mail->setFrom('dammyfyp@gmail.com', 'Password Reset');
             $mail->addAddress($_POST["email"]);
-            //$mail->addReplyTo('lamkaizhe16@gmail.com');
 
             // HTML body
             $mail->isHTML(true);
@@ -125,7 +112,7 @@
             <br><br>
             <p>With regrads,</p>
             <b>SBC</b>";
-
+            $mail->send();
             if(!$mail->send()){
                 ?>
                     <script>
@@ -141,6 +128,4 @@
             }
         }
     }
-
-
 ?>

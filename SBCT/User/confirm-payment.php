94d31ffd876139a6ed96bda6e94030dcb3766b4c
ALicia9j <106652230+ALicia9j@users.noreply.github.com>
2023-07-31 17:33:31 +0800

    Update confirm-payment.php

--- SBCT/User/confirm-payment.php
+++ SBCT/User/confirm-payment.php
@@ -30,7 +30,7 @@
     <link rel="icon" type="image/jpg" href="../images/logo-dark.jpg">
     <link rel="stylesheet" href="user-style.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
-    <link rel="stylesheet" href="font-awesome-4.7.0/css/font-awesome.min.css" type="text/css">
+    <link rel="stylesheet" href="../font-awesome-4.7.0/css/font-awesome.min.css" type="text/css">
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"></script>
 </head>

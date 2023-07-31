6a177379424b3981631e73a4a4b20db309bf8719
ALicia9j <106652230+ALicia9j@users.noreply.github.com>
2023-07-31 11:24:11 +0800

    Update terms-of-use.php

--- SBCT/terms-of-use.php
+++ SBCT/terms-of-use.php
@@ -7,6 +7,7 @@
 <html lang="en">
 <head>
     <title>SBC | Terms of Use</title>
+    <link rel="icon" type="image/jpg" href="images/logo-dark.jpg">
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
     <link rel="stylesheet" href="font-awesome-4.7.0/css/font-awesome.min.css" type="text/css">

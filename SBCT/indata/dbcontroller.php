0228d2b52c43caf990752e93c2098cb1f5aa14db
divya <106322090+divya21025496@users.noreply.github.com>
2023-08-01 12:16:49 +0800

    Update dbcontroller.php

--- SBCT/indata/dbcontroller.php
+++ SBCT/indata/dbcontroller.php
@@ -1,6 +1,6 @@
 <?php
     class DBController {
-        private $host = "196.16.0.3";
+        private $host = "196.16.0.2";
         private $user = "sbc";
         private $pass = "fyp";
         private $dbname = "sbc";

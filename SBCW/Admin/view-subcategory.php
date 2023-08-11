6657e2823133f656103930848ac1e88af04ea2d3
ALicia9j <106652230+ALicia9j@users.noreply.github.com>
2023-08-11 22:02:08 +0800

    Update view-subcategory.php

--- SBCW/Admin/view-subcategory.php
+++ SBCW/Admin/view-subcategory.php
@@ -1,7 +1,7 @@
 <h3 class="text-center" style="color:#C65C81;">ALL Sub Categories</h3>
 <!--- Subcategory table-->
 <table class="table table-bordered mt-5 text-center">
-    <thead style="background: #00BFFF; color:white;">
+    <thead style="background: #E799A3; color:white;">
         <tr>
             <th>Subcategory ID</th>
             <th>Subcategory Title</th>
